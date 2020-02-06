e961c3722cab87fd4d7bc003466be28e6e946a83
Igor Nadj <igor@ethicaljobs.com.au>
2020-02-06 21:24:52 +1100

    Pretty render, first solution

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const EGG = 'EGG';
+const SNAKE = 'SNAKE';
 
 const initial = [
 	[null, null, EGG,  null, null, null, null, null, null, null],
@@ -13,37 +14,77 @@ const initial = [
 	[null, null, null, null, null, null, null, null, null, null],
 ];
 
-render(initial, []);
+const solution = [
+	[9, 9, null, SNAKE, SNAKE, 7, SNAKE, SNAKE, SNAKE, SNAKE],
+	[9, 9, 9, 9, SNAKE, 7, SNAKE, 5, 5, SNAKE],
+	[null, SNAKE, SNAKE, 9, SNAKE, 7, SNAKE, SNAKE, 5, SNAKE],
+	[2, 2, SNAKE, 9, SNAKE, 7, 7, SNAKE, 5, SNAKE],
+	[SNAKE, SNAKE, SNAKE, 9, SNAKE, 7, SNAKE, SNAKE, 5, SNAKE],
+	[SNAKE, 8, 8, SNAKE, SNAKE, 7, SNAKE, 4, SNAKE, SNAKE],
+	[SNAKE, SNAKE, 8, SNAKE, 1, SNAKE, SNAKE, 4, SNAKE, 6],
+	[3, SNAKE, 8, SNAKE, SNAKE, SNAKE, 4, 4, SNAKE, 6],
+	[3, SNAKE, 8, 8, 8, 8, SNAKE, SNAKE, SNAKE, 6],	
+	[3, SNAKE, SNAKE, SNAKE, SNAKE, SNAKE, SNAKE, 6, 6, 6],
+];
+
+render(combine(initial, solution));
 
 
-function check(initial, solution) {
-	const combined = {...solution, ...initial}; // merge initial on top so we don't allow overriding the initial
+function check(solution) {
 	return true; // stub
 }
 
-function render(initial, solution) {
-	const combined = {...solution, ...initial}; // merge initial on top so we don't allow overriding the initial
+function render(solution) {
+	const combined = {...solution, ...initial};
 	const gridEl = document.getElementById('grid');
 	let newHtml = '<table>';
 	for (let row = 1; row <= 10; row++) {
 		newHtml += '<tr>';
 		for (let col = 1; col <= 10; col++) {
-			newHtml += '<td class="cell">';
+			let cellContent = ""
+			let cellClass = "cell "; 
 			switch (combined[row-1][col-1]) {
 				case null:
-					newHtml += '';
+					cellContent = '';
+					cellClass += "empty";
 					break;
 				case EGG:
-					newHtml += 'O';
+					cellContent = 'O';
+					cellClass += "egg"
+					break;
+				case SNAKE:
+					cellClass += "snake";
 					break;
 				default:
-					newHtml += combined[row-1][col-1];
+					cellClass = "water";
+					cellContent = combined[row-1][col-1];
 			}
-			newHtml += '</td>';
+			newHtml += '<td class="' + cellClass + '">' + cellContent + '</td>';
 		}
 		newHtml += '</tr>';
 	}
 	gridEl.innerHTML = newHtml;
 }
+
+function combine(initial, solution) {
+	// merge initial on top so we don't override the initial
+	const combined = initial;
+	for (let row = 1; row <= 10; row++) {
+		for (let col = 1; col <= 10; col++) {
+			if (initial[row-1][col-1] === null) {
+				combined[row-1][col-1] = solution[row-1][col-1];
+			}
+		}
+	}
+	return combined;
+}
+
+
+
+
+
+
+
+
 
 
