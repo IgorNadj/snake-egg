ca47fb9d4ac4aa6e797f2799da7c5fbb7cc6833d
Igor Nadj <igor@ethicaljobs.com.au>
2020-02-28 11:54:56 +1100

    Move generic puzzle functions to generic puzzle class

---
+++ test/PuzzleTest.ts
@@ -0,0 +1,96 @@
+import { expect } from 'chai';
+import { Polyomino, PointInt } from 'polyomino';
+import { GridCell, Neighbours, Puzzle } from '../src/Puzzle';
+import { PlacedPolyomino } from '../src/polyomino/PlacedPolyomino';
+import { Set } from 'immutable';
+
+
+describe("Puzzle", () => {
+
+  it("getGrid works", () => {
+    const puzzle = getPuzzle(2, 2, 1, [[0, 0]]);
+
+    const grid = puzzle.getGrid();
+
+    const expected = [
+      [GridCell.POLY, GridCell.SNAKE],
+      [GridCell.SNAKE, GridCell.SNAKE],
+    ];
+
+    expect(grid).to.deep.equal(expected);
+  });
+
+  it("getGridNeighbours works", () => {
+    /*
+     * 1 .
+     * . .
+     */
+
+    const puzzle = getPuzzle(2, 2, 1, [[0, 0]]);
+
+    const grid = puzzle.getGrid();
+
+    const neighbours = puzzle.getGridNeighbours(grid, 1, 0);
+
+    const expected = {
+      top: null,
+      right: null,
+      bottom: GridCell.SNAKE,
+      left: GridCell.POLY,
+      topRight: null,
+      bottomRight: null,
+      bottomLeft: GridCell.SNAKE,
+      topLeft: null,
+    };
+
+    expect(neighbours).to.deep.equal(expected);
+  });
+
+  it("countSnakeAdjacentSegments works", () => {
+    /*
+     * . 1 .
+     * 1 . .
+     * . . 1
+     */
+    // Given the target point is in the middle, it should only count horizontal 
+    // and vertical adjacent, so there are 2 snake segments adjacent. 
+
+    const puzzle = getPuzzle(3, 3, 1, [[0, 1], [1, 0], [2, 2]]);
+
+    const neighbours: Neighbours = {
+      top: GridCell.POLY,
+      right: GridCell.SNAKE,
+      bottom: GridCell.SNAKE,
+      left: GridCell.POLY,
+      topRight: GridCell.SNAKE,
+      bottomRight: GridCell.POLY,
+      bottomLeft: GridCell.SNAKE,
+      topLeft: GridCell.SNAKE,
+    }
+
+    expect(puzzle.countSnakeAdjacentSegments(neighbours)).to.equal(2);
+  });
+
+  it("getSnakeLength works", () => {
+    /*
+      * . . 1 .
+      * 1 . . .
+      */
+    const puzzle = getPuzzle(4, 2, 1, [[2, 0], [0, 1]]);
+
+    expect(puzzle.getSnakeLength(new PointInt(0, 0))).to.equal(6);
+  });
+
+  function getPuzzle(width: number, height: number, maxNum: number, polyPlacements: number[][]): Puzzle {
+    const poly = Polyomino.get(1).first();
+
+    let placedPolyominos: Set<PlacedPolyomino> = Set();
+
+    polyPlacements.forEach((placement) => {
+        placedPolyominos = placedPolyominos.add(new PlacedPolyomino(poly, placement[0], placement[1]))
+    });
+
+    return new Puzzle(width, height, maxNum, placedPolyominos);
+  }
+
+});
--- test/generator/GeneratingPuzzleTest.ts
+++ test/generator/GeneratingPuzzleTest.ts
@@ -1,7 +1,6 @@
 import { expect } from 'chai';
 import { Polyomino, PointInt } from 'polyomino';
 import { GeneratingPuzzle } from "../../src/generator/GeneratingPuzzle";
-import { GridCell, Neighbours } from '../../src/Puzzle';
 
 
 describe("GeneratingPuzzle", () => {
@@ -14,93 +13,6 @@ describe("GeneratingPuzzle", () => {
     const newPuzzle = originalPuzzle.place(poly, 1, 1);
 
     expect(originalPuzzle === newPuzzle).to.equal(false);
-  });
-
-  it("getGrid works", () => {
-    let puzzle = new GeneratingPuzzle(2, 2, 1);
-
-    const poly = Polyomino.get(1).first();
-
-    puzzle = puzzle.place(poly, 0, 0);
-
-    const grid = puzzle.getGrid();
-
-    const expected = [
-      [GridCell.POLY, GridCell.SNAKE],
-      [GridCell.SNAKE, GridCell.SNAKE],
-    ];
-
-    expect(grid).to.deep.equal(expected);
-  });
-
-  it("getGridNeighbours works", () => {
-    /*
-     * 1 .
-     * . .
-     */
-
-    let puzzle = new GeneratingPuzzle(2, 2, 1);
-
-    const poly = Polyomino.get(1).first();
-
-    puzzle = puzzle.place(poly, 0, 0);
-
-    const grid = puzzle.getGrid();
-
-    const neighbours = puzzle.getGridNeighbours(grid, 1, 0);
-
-    const expected = {
-      top: null,
-      right: null,
-      bottom: GridCell.SNAKE,
-      left: GridCell.POLY,
-      topRight: null,
-      bottomRight: null,
-      bottomLeft: GridCell.SNAKE,
-      topLeft: null,
-    };
-
-    expect(neighbours).to.deep.equal(expected);
-  });
-
-  it("countSnakeAdjacentSegments works", () => {
-    const puzzle = new GeneratingPuzzle(3, 3, 1);
-
-    /*
-     * . 1 .
-     * 1 . .
-     * . . 1
-     */
-    // Given the target point is in the middle, it should only count horizontal 
-    // and vertical adjacent, so there are 2 snake segments adjacent. 
-
-    const neighbours: Neighbours = {
-      top: GridCell.POLY,
-      right: GridCell.SNAKE,
-      bottom: GridCell.SNAKE,
-      left: GridCell.POLY,
-      topRight: GridCell.SNAKE,
-      bottomRight: GridCell.POLY,
-      bottomLeft: GridCell.SNAKE,
-      topLeft: GridCell.SNAKE,
-    }
-
-    expect(puzzle.countSnakeAdjacentSegments(neighbours)).to.equal(2);
-  });
-
-  it("getSnakeLength works", () => {
-    /*
-      * . . 1 .
-      * 1 . . .
-      */
-    let puzzle = new GeneratingPuzzle(4, 2, 1);
-
-    const poly = Polyomino.get(1).first();
-
-    puzzle = puzzle.place(poly, 2, 0);
-    puzzle = puzzle.place(poly, 0, 1);
-
-    expect(puzzle.getSnakeLength(new PointInt(0, 0))).to.equal(6);
   });
 
 });
