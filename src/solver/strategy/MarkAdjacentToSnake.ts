9cabbd36e41f6c3a220eae0d18b8cf6d460a10e7
Igor Nadj <igor.nadj@gmail.com>
2020-08-29 23:53:02 +1000

    Fix MarkAdjacentToSnake, simpler logic

--- src/solver/strategy/MarkAdjacentToSnake.ts
+++ src/solver/strategy/MarkAdjacentToSnake.ts
@@ -1,39 +1,31 @@
-import { Strategy } from "./Strategy";
-import { SolvingPuzzle } from "../SolvingPuzzle";
-import { GridCell } from "../../Puzzle";
-import { PuzzleHelper } from "../../helper/PuzzleHelper";
+import {Strategy} from "./Strategy";
+import {SolvingPuzzle} from "../SolvingPuzzle";
+import {GridCell} from "../../Puzzle";
+import {PuzzleHelper} from "../../helper/PuzzleHelper";
 
 export class MarkAdjacentToSnake implements Strategy {
 
     public solve(puzzle: SolvingPuzzle): SolvingPuzzle {
 
-        const snakeEnds = PuzzleHelper.getSnakeEnds(puzzle.getSolveGrid());
+        const bodyCells = PuzzleHelper.getSnakeBody(puzzle.getSolveGrid());
 
-        for (const snakeEnd of snakeEnds) {
-            const x = snakeEnd.x;
-            const y = snakeEnd.y;
+        for (const bodyCell of bodyCells) {
+            const x = bodyCell.x;
+            const y = bodyCell.y;
 
             const solveNeighbours = puzzle.getSolveGrid().getGridNeighbours(x, y);
 
-            if (solveNeighbours.top === GridCell.SNAKE) {
-                // we came from above, topRight and topLeft must be POLY
-                puzzle = puzzle.solve(x - 1, y - 1, GridCell.POLY);
-                puzzle = puzzle.solve(x + 1, y - 1, GridCell.POLY);
+            if (solveNeighbours.top === null) {
+                puzzle = puzzle.solve(x, y - 1, GridCell.POLY);
             }
-            if (solveNeighbours.right === GridCell.SNAKE) {
-                // we came from right, topRight and bottomRight must be POLY
-                puzzle = puzzle.solve(x + 1, y - 1, GridCell.POLY);
-                puzzle = puzzle.solve(x + 1, y + 1, GridCell.POLY);
+            if (solveNeighbours.right === null) {
+                puzzle = puzzle.solve(x + 1, y, GridCell.POLY);
             }
-            if (solveNeighbours.bottom === GridCell.SNAKE) {
-                // we came from below, bottomRight and bottomLeft must be POLY
-                puzzle = puzzle.solve(x - 1, y + 1, GridCell.POLY);
-                puzzle = puzzle.solve(x + 1, y + 1, GridCell.POLY);
+            if (solveNeighbours.bottom === null) {
+                puzzle = puzzle.solve(x, y + 1, GridCell.POLY);
             }
-            if (solveNeighbours.left === GridCell.SNAKE) {
-                // we came from left, topLeft and bottomLeft must be POLY
-                puzzle = puzzle.solve(x - 1, y - 1, GridCell.POLY);
-                puzzle = puzzle.solve(x - 1, y + 1, GridCell.POLY);
+            if (solveNeighbours.left === null) {
+                puzzle = puzzle.solve(x - 1, y, GridCell.POLY);
             }
         }
 
--- test/solver/strategy/MarkAdjacentToSnakeTest.ts
+++ test/solver/strategy/MarkAdjacentToSnakeTest.ts
@@ -3,12 +3,11 @@ import { GridCell } from '../../../src/Puzzle';
 import { SolvingPuzzle, SolveCell } from "../../../src/solver/SolvingPuzzle";
 import { MarkAdjacentToSnake } from '../../../src/solver/strategy/MarkAdjacentToSnake';
 import { Grid } from '../../../src/grid/Grid';
-import { HintedPuzzle } from '../../../src/hinter/HintedPuzzle';
-
+import {HintedPuzzle, EGG, HintCell} from '../../../src/hinter/HintedPuzzle';
 
 describe("MarkAdjacentToSnake", () => {
 
-  it("marks adjacent as poly when moving straight", () => {
+  it("marks cells orthogonal to snake body as poly (straight)", () => {
     /*
     * Given puzzle:
     * 
@@ -49,23 +48,21 @@ describe("MarkAdjacentToSnake", () => {
     expect(solved.getSolveGrid().toArray()).to.deep.equal(expectedSolveGrid);
   });
 
-  it("marks adjacent as poly when turning", () => {
+  it("marks orthogonal to snake body as poly (turning)", () => {
     /*
     * Given puzzle:
-    * 
+    *
     *   _ _ _ _ _
     *   P P . _ _
     *   . . . _ _
-    * 
+    *
     * we can mark where it didn't go as poly:
-    * 
+    *
     *   _ _ _ _ _
     *   P P . _ _
     *   . . . P _
     */
-    let solveGrid: Grid<SolveCell> = new Grid(5, 3);
-
-    solveGrid = solveGrid.fromArray([
+    const solveGrid = new Grid<SolveCell>(5, 3, [
       [null, null, null, null, null],
       [GridCell.POLY, GridCell.POLY, GridCell.SNAKE, null, null],
       [GridCell.SNAKE, GridCell.SNAKE, GridCell.SNAKE, null, null],
@@ -73,17 +70,48 @@ describe("MarkAdjacentToSnake", () => {
 
     const puzzle = new SolvingPuzzle(new HintedPuzzle(5, 3, 5), solveGrid);
 
-    const expectedSolveGrid = [
+    const expectedGrid = new Grid<SolveCell>(5, 3, [
       [null, null, null, null, null],
       [GridCell.POLY, GridCell.POLY, GridCell.SNAKE, null, null],
       [GridCell.SNAKE, GridCell.SNAKE, GridCell.SNAKE, GridCell.POLY, null],
-    ];
+    ]);
 
     const strategy = new MarkAdjacentToSnake();
 
     const solved = strategy.solve(puzzle);
 
-    expect(solved.getSolveGrid().toArray()).to.deep.equal(expectedSolveGrid);
+    expect(solved.getSolveGrid().equals(expectedGrid)).to.be.true;
+  });
+
+  it("only marks orthogonal to snake body, not head", () => {
+    /*
+    * Given puzzle:
+    *
+    *   _ _ _ _ _
+    *   _ _ _ _ .
+    *   _ _ _ P o
+    *
+    * the head could go left or up, so dont mark left or up as poly
+    */
+    const solveGrid = new Grid<HintCell>(5, 3, [
+      [null, null, null, null, null],
+      [null, null, null, null, GridCell.SNAKE],
+      [null, null, null, GridCell.POLY, EGG],
+    ]);
+
+    const puzzle = new SolvingPuzzle(new HintedPuzzle(5, 3, 5), solveGrid);
+
+    const expectedGrid = new Grid<HintCell>(5, 3, [
+      [null, null, null, null, null],
+      [null, null, null, null, GridCell.SNAKE],
+      [null, null, null, GridCell.POLY, EGG],
+    ]);
+
+    const strategy = new MarkAdjacentToSnake();
+
+    const solved = strategy.solve(puzzle);
+
+    expect(solved.getSolveGrid().equals(expectedGrid)).to.be.true;
   });
 
 });
