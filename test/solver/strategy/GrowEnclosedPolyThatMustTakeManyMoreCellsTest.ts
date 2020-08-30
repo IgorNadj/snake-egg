699c17b91ebd9321837b5aca56a6c348411b1a3f
Igor Nadj <igor.nadj@gmail.com>
2020-08-30 15:46:20 +1000

    Add strategy test stub

---
+++ test/solver/strategy/GrowEnclosedPolyThatMustTakeManyMoreCellsTest.ts
@@ -0,0 +1,24 @@
+
+
+describe.skip("GrowEnclosedPolyThatMustTakeManyMoreCells", () => {
+
+  it("fills cells that a poly must take", () => {
+    /*
+    * Given puzzle:
+    * 
+    *   . 4 _ .
+    *   . _ _ .
+    *   . . _ .
+    *   _ _ _ _
+    *
+    * we dont know how the 5 region grows, but it must at least take the gap:
+    * 
+    *   . 4 _ .
+    *   . _ 4 .
+    *   . . _ .
+    *   _ _ _ _
+    *
+    */
+  });
+
+});
